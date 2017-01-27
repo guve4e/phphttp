0070e43653361dafea88a4dde2180af6d086b112
Valentin Kormanov <valentin.kormanov@gmail.com>
2017-01-26 21:06:50 -0500

    Added Example

---
+++ example/example.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once("../http/RestCall.php");
+
+try {
+	// create instance 
+    $r = RestCall::create();
+    // set URL
+    $r->setUrl("http://carstuff.ddns.net/web-api/index.php/Test");
+    // set Content Type
+    $r->setContentType("application/json");
+    // set Method
+    $r->setMethod("PUT ");
+
+    // sample data 
+    $json_data = [
+        "name" => "John",
+        "occupation" => "Programmer"
+    ];
+
+    // set to JSON format
+    $r->setJsonData($json_data);
+
+    // make headers
+    $headers = [
+        "a" => "b",
+        "c" => "d"
+    ];
+
+    // set headers
+    $r->setHeaders($headers);
+
+    // send the request
+    $s = $r->sendRequest();
+    // print the result
+    print_r($s);
+    
+} 	catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+?>
+ 
